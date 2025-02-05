b26399fba15f8e98f5392e1f214410c614679f88
Michal Mrnustik <michal.mrnustik@outlook.com>
2025-02-05 22:04:55 +0100

    fix: add missing health check services registration

--- src/backend/Habitator.API/Program.cs
+++ src/backend/Habitator.API/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
