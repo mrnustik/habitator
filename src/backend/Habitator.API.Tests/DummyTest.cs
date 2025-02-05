f5263369cc9e858fff160d401ae9152e11f7a90f
Michal Mrnustik <michal.mrnustik@outlook.com>
2025-02-05 21:49:53 +0100

    chore: add initial dotnet API and test project

---
+++ src/backend/Habitator.API.Tests/DummyTest.cs
@@ -0,0 +1,10 @@
+namespace Habitator.API.Tests;
+
+public class DummyTest
+{
+    [Fact]
+    public void DummyTest_ShouldAlwaysPass()
+    {
+        Assert.True(true);
+    }
+}
---
+++ src/backend/Habitator.API/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapHealthChecks("/health");
+app.MapHealthChecks("/health/alive", new HealthCheckOptions
+{
+    Predicate = r => r.Tags.Contains("alive"),
+});
+
+app.Run();
