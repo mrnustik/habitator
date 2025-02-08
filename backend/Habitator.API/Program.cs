adcacdc80cb03a0093781a8a684b61c33314e667
Michal Mrnustik <michal.mrnustik@outlook.com>
2025-02-08 17:09:04 +0100

    feat: add OpenTelemetry exporting from the Habitator.API

--- backend/Habitator.API/Program.cs
+++ backend/Habitator.API/Program.cs
@@ -1,6 +1,51 @@
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var oltpEndpoint = builder.Configuration["OTLP_ENDPOINT_URL"];
+
+var openTelemetryBuilder = builder.Services.AddOpenTelemetry();
+
+openTelemetryBuilder.ConfigureResource(r => r.AddService("habitator-api"));
+
+openTelemetryBuilder.WithMetrics(m =>
+                                     m.AddAspNetCoreInstrumentation()
+                                      .AddRuntimeInstrumentation()
+                                      .AddProcessInstrumentation()
+                                      .AddMeter("*")
+                                      .AddPrometheusExporter());
+
+openTelemetryBuilder.WithTracing(t =>
+{
+    t.AddAspNetCoreInstrumentation();
+    if (!string.IsNullOrEmpty(oltpEndpoint))
+    {
+        t.AddOtlpExporter(e =>
+        {
+            e.Protocol = OtlpExportProtocol.Grpc;
+            e.Endpoint = new Uri(oltpEndpoint);
+        });
+    }
+});
+
+builder.Logging.AddOpenTelemetry(o =>
+{
+    o.IncludeScopes = true;
+
+    if (!string.IsNullOrEmpty(oltpEndpoint))
+    {
+        o.AddOtlpExporter(e =>
+        {
+            e.Protocol = OtlpExportProtocol.Grpc;
+            e.Endpoint = new Uri(oltpEndpoint);
+        });
+    }
+});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,5 +65,7 @@ app.MapHealthChecks("/health/alive", new HealthCheckOptions
 {
     Predicate = r => r.Tags.Contains("alive"),
 });
+
+app.MapPrometheusScrapingEndpoint();
 
 app.Run();
